101dc4b4b0421b546b686dc612ba086b4213c258
Edgardosilva <edgardosilva.es@gmail.com>
2024-02-16 19:00:21 -0300

    Add CardWithDescription component

---
+++ src/app/components/CardWithDescription.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const CardWithDescription = ({ img, title, date, location }) => {
+  return (
+    <div className=" flex max-w-3xl bg-white border border-blue-500 rounded-lg shadow-xl ">
+      <Image
+        src={img || "/imgCard-1.png"}
+        alt="card image"
+        width={350}
+        height={400}
+      />
+      <div className="p-5 flex flex-col justify-center">
+        <h5 class="mb-2 text-xl font-bold tracking-tight ">
+          {title || "Titulo del evento"}
+        </h5>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non
+          sodales lacus, sit amet pretium massa. Fusce sodales ante leo, vitae
+          placerat neque egestas at.
+        </p>
+        <div className="flex items-center mb-2">
+          <Image
+            src="/calendarIcon.svg"
+            alt="card image"
+            width={20}
+            height={20}
+            className="mr-2"
+          />
+          <p className="font-normal text-[#FF256F] ">
+            {date || "Fecha del evento"}
+          </p>
+        </div>
+        <div className="flex items-center">
+          <Image
+            src="/locationLogo.svg"
+            alt="card image"
+            width={20}
+            height={20}
+            className="mr-2"
+          />
+          <p className="font-bold text-[#23B0FF] ">
+            {location || "Ubicación del evento"}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardWithDescription;
