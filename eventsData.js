d1819271e328fe89b650e66866eee97df5396d9b
Edgardosilva <edgardosilva.es@gmail.com>
2024-02-19 18:14:16 -0300

    Add eventsData.js file with example event data

---
+++ eventsData.js
@@ -0,0 +1,39 @@
+export const eventsData = [
+    {
+      "img": "/images/imgCard-1.png",
+      "title": "Mercados Monetarios (Forex & Monedas Digitales)",
+      "date": "02/03/2024 - 7:00 PM (GMT-5)",
+      "location": "Lima, PE"
+    },
+    {
+      "img": "/images/imgCard-2.png",
+      "title": "Mercados Monetarios (Forex & Monedas Digitales)",
+      "date": "02/03/2024 - 7:00 PM (GMT-5)",
+      "location": "Lima, PE"
+    },
+    {
+      "img": "/images/imgCard-3.png",
+      "title": "Mercados Monetarios (Forex & Monedas Digitales)",
+      "date": "02/03/2024 - 7:00 PM (GMT-5)",
+      "location": "Lima, PE"
+    },
+    {
+      "img": "/images/imgCard-1.png",
+      "title": "Mercados Monetarios (Forex & Monedas Digitales)",
+      "date": "02/03/2024 - 7:00 PM (GMT-5)",
+      "location": "Lima, PE"
+    },
+    {
+      "img": "/images/imgCard-1.png",
+      "title": "Mercados Monetarios (Forex & Monedas Digitales)",
+      "date": "02/03/2024 - 7:00 PM (GMT-5)",
+      "location": "Lima, PE"
+    },
+    {
+      "img": "/images/imgCard-1.png",
+      "title": "Mercados Monetarios (Forex & Monedas Digitales)",
+      "date": "02/03/2024 - 7:00 PM (GMT-5)",
+      "location": "Lima, PE"
+    }
+  ]
+  
