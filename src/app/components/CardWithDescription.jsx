4072592b4b08f5e65632a357edf0935fe18bd7bd
Edgardosilva <edgardosilva.es@gmail.com>
2024-02-22 12:31:18 -0300

    Refactor height  cardwidthdescription component

--- src/app/components/CardWithDescription.jsx
+++ src/app/components/CardWithDescription.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 
 const CardWithDescription = ({ img, title, date, type }) => {
   return (
@@ -12,47 +11,49 @@ const CardWithDescription = ({ img, title, date, type }) => {
         height={300}
         className="rounded-lg"
       />
-      <div className="p-5 flex flex-col justify-center ">
+      <div className="p-5 flex flex-col justify-center">
         <h5 class="mb-2 text-xl font-bold tracking-tight ">
           {title || "Titulo del evento"}
         </h5>
-        <p>
+        <p className=" leading-5">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non
           sodales lacus, sit amet pretium massa. Fusce sodales ante leo, vitae
           placerat neque egestas at.
         </p>
-        <div className="flex items-center mb-2">
-          <Image
-            src="/calendarIcon.svg"
-            alt="card image"
-            width={20}
-            height={20}
-            className="mr-2"
-          />
-          <p className="font-normal text-[#FF256F] ">
-            {date || "Fecha del evento"}
-          </p>
-        </div>
-        <div className="flex items-center">
-          {type === "Online"
-          ? <Image
-              src="/camera-reels-fill.svg"
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center pt-3">
+            <Image
+              src="/calendarIcon.svg"
               alt="card image"
               width={20}
               height={20}
               className="mr-2"
             />
-          : <Image
-              src="/locationLogo.svg"
-              alt="card image"
-              width={20}
-              height={20}
-              className="mr-2"
-            />
-          }
-          <p className={`font-bold text-[#23B0FF] ${type === 'Online' ? 'text-[#25CC68]' : ''}`}>
-            {type || "Ubicación del evento"}
-          </p>
+            <p className="font-bold text-[#FF256F] ">
+              {date || "Fecha del evento"}
+            </p>
+          </div>
+          <div className="flex items-center">
+            {type === "Online"
+            ? <Image
+                src="/camera-reels-fill.svg"
+                alt="card image"
+                width={20}
+                height={20}
+                className="mr-2"
+              />
+            : <Image
+                src="/locationLogo.svg"
+                alt="card image"
+                width={20}
+                height={20}
+                className="mr-2"
+              />
+            }
+            <p className={`font-bold text-[#23B0FF] ${type === 'Online' ? 'text-[#25CC68]' : ''}`}>
+              {type || "Ubicación del evento"}
+            </p>
+          </div>
         </div>
       </div>
     </div>
