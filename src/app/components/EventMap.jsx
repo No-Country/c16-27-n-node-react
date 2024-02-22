12760736316e7a579d5d612e0debb3ade8a48711
Edgardosilva <edgardosilva.es@gmail.com>
2024-02-21 22:09:56 -0300

    Add EventMap component for displaying event location on a map

---
+++ src/app/components/EventMap.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+const EventMap = ({ eventData }) => {
+  return (
+    <>
+      <article className="p-2">
+        <h2 className="text-[#23B0FF] font-bold text-xl">
+          {eventData.address}
+        </h2>
+      </article>
+      <article className=" h-[350px] rounded-lg p-5">
+        {eventData.type === "Presencial" ? (
+          <iframe
+            width="400"
+            height="350"
+            loading="lazy"
+            allowfullscreen
+            referrerpolicy="no-referrer-when-downgrade"
+            src={`https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=${eventData.address}`}
+          ></iframe>
+        ) : (
+          <p>Evento en línea</p>
+        )}
+      </article>
+    </>
+  );
+};
+
+export default EventMap;
