f0407f23814825081f7eb1eb6351c9b2f0988616
Edgardosilva <edgardosilva.es@gmail.com>
2024-02-21 22:11:07 -0300

    Add CalendarDate component

---
+++ src/app/components/CalendarDate.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Image from 'next/image';
+
+const CalendarDate = ({ eventData }) => {
+  return (
+    <>
+      <Image
+        src={"/calendarIcon.svg"}
+        alt={`calentarIcon del evento`}
+        width={30}
+        height={30}
+      />
+      <h1 className="text-[#FF256F] text-2xl font-bold">
+        {eventData.date}
+      </h1>
+    </>
+  );
+};
+
+export default CalendarDate; 
