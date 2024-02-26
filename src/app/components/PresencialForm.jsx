b0571e6893baacb9e14acab3cd042d7104fb0395
Edgardosilva <edgardosilva.es@gmail.com>
2024-02-26 18:16:17 -0300

    Add EventFormMap component and address state

--- src/app/components/PresencialForm.jsx
+++ src/app/components/PresencialForm.jsx
@@ -1,7 +1,13 @@
+'use client'
 import React from 'react';
 import Image from 'next/image';
+import EventFormMap from '@/app/components/EventFormMap';
+import { useState } from 'react';
 
 const PresencialForm = () => {
+
+  const [address, setAddress] = useState('')
+
   return (
     <form action="/ruta/del/endpoint" method="POST" className="flex flex-col justify-between">
       <section className='flex w-full text-lg p-3 h-[60rem]'>
@@ -27,29 +33,29 @@ const PresencialForm = () => {
             className="border h-1/2 p-5"
           ></textarea>
         </div>
-        <div className="flex flex-col border-l w-1/2 p-4">
+        <div className="flex flex-col border-l w-1/2 p-4 justify-between">
           <div className="h-1/2 flex flex-col gap-2">
             <label htmlFor="">Ciudad</label>
             <select name="" id="" className="border rounded-md"></select>
             <label htmlFor="">Dirección</label>
-            <input type="text" className="border rounded-md" />
+            <input 
+              type="text" 
+              className="border rounded-md"
+              placeholder='Buscar dirección en el mapa'
+              value={address} 
+              onChange={(e) => setAddress(e.target.value)}
+            />
             <div className="h-full flex items-center justify-center">
-              <Image
-                src={"/mapsImage.png"}
-                alt="card image"
-                width={400}
-                height={300}
-                className="rounded-lg"
-              />
+             <EventFormMap address={address || '15.5126501,-80.5271313,3'} />
             </div>
           </div>
-          <div className="border h-1/2 bg-[#C4C4C4] flex items-center justify-center rounded-lg">
+          <div className="border h-2/5 bg-[#C4C4C4] flex items-center justify-center rounded-lg">
             <label htmlFor="fileInput" className="cursor-pointer">
               <Image
                 src={"/camera-fill.svg"}
                 alt="icono de la cámara"
                 width={300}
-                height={300}
+                height={200}
                 className="rounded-lg"
               />
               <input
