ea2cbce5d1c5441ea3db2a08802be427252e7462
ajmariano98 <aj_mariano1998@outlook.com>
2024-02-27 21:43:03 -0300

    Add categories and timezones

---
+++ categoriesData.js
@@ -0,0 +1,7 @@
+export const categories = [
+  { value: 1, name: "Salud" },
+  { value: 2, name: "Negocios" },
+  { value: 3, name: "Tecnología" },
+  { value: 4, name: "Mascotas" },
+  { value: 5, name: "Comunidad" },
+];
