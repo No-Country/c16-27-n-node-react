d1619f4cfca7146d104e3173ebde72c7924a9a9c
ajmariano98 <aj_mariano1998@outlook.com>
2024-02-28 15:06:21 -0300

    Add categories and gmt in PresencialForm and event/id

--- eventsData.js
+++ eventsData.js
@@ -4,6 +4,7 @@ export const eventsData = [
     creatorUserId: 1,
     img: "/images/event1img.jpg",
     title: "Mercados Monetarios (Forex & Monedas Digitales)",
+    category: 2,
     description:
       "Únete a nosotros en una inmersión profunda en el emocionante y dinámico mundo de los mercados monetarios. Desde las fluctuaciones del Forex hasta la innovación de las criptomonedas, este evento presencial está diseñado para ofrecerte una visión única y valiosa de las tendencias y estrategias que están dando forma al panorama financiero global. Durante este evento de dos días, los participantes tendrán la oportunidad de interactuar con expertos en Forex y criptomonedas, así como con líderes de opinión de la industria financiera. Desde sesiones magistrales hasta paneles de discusión, exploraremos temas que van desde la teoría económica subyacente hasta las estrategias prácticas de inversión. Descubre cómo las divisas tradicionales se entrelazan con las nuevas formas de dinero digital, y cómo puedes capitalizar estas interacciones para diversificar tu cartera y maximizar tus oportunidades de inversión. Aprende sobre las últimas tecnologías, tendencias regulatorias y análisis de mercado que están moldeando el futuro de las finanzas. Ya seas un operador experimentado en Forex, un entusiasta de las criptomonedas o simplemente alguien interesado en explorar nuevas oportunidades de inversión, este evento es tu ventana al mundo de los mercados monetarios. Únete a nosotros mientras desentrañamos los secretos de los mercados financieros y te equipamos con el conocimiento y las herramientas necesarias para navegar con confianza en este emocionante paisaje económico. ¡Reserva tu lugar hoy y prepárate para sumergirte en una experiencia enriquecedora que transformará tu comprensión de los mercados monetarios para siempre!",
     date: "18/03/2024 19:00 GMT-5",
@@ -17,6 +18,7 @@ export const eventsData = [
     creatorUserId: 2,
     img: "/images/event2img.webp",
     title: "Open Beer Bogotá 2024",
+    category: 5,
     description:
       "¡Únete a nosotros en el evento de cerveza más emocionante del año! Open Beer Bogotá 2024 es una celebración de la cultura cervecera que no te puedes perder. Prepárate para disfrutar de una amplia selección de cervezas artesanales, música en vivo, deliciosa comida callejera y un ambiente lleno de energía y camaradería. Desde los clásicos favoritos hasta las últimas novedades en el mundo de la cerveza, este evento es perfecto para los amantes de la cerveza de todos los niveles. ¡Ven y brinda con nosotros en Open Beer Bogotá 2024!",
     date: "21-03-2024 09:00 GMT-5",
@@ -30,6 +32,7 @@ export const eventsData = [
     creatorUserId: 3,
     img: "/images/event3img.png",
     title: "Inteligencia Artificial: una mirada desde y para la universidad",
+    category: 3,
     description:
       "Explora el fascinante mundo de la inteligencia artificial desde una perspectiva académica en nuestro próximo evento en línea. Profundizaremos en los avances más recientes en inteligencia artificial y su impacto en el ámbito universitario y más allá. Desde algoritmos de aprendizaje automático hasta aplicaciones prácticas en diferentes disciplinas, nuestros expertos compartirán conocimientos y experiencias sobre este emocionante campo. Esta sesión es ideal tanto para estudiantes universitarios interesados en la IA como para profesionales en busca de nuevas perspectivas y oportunidades. Únete a nosotros para una exploración enriquecedora de uno de los campos más emocionantes de la ciencia y la tecnología.",
     date: "24-03-2024 19:00 GMT-3",
@@ -43,6 +46,7 @@ export const eventsData = [
     creatorUserId: 4,
     img: "/images/event4img.webp",
     title: "Coffee Portuguese and English",
+    category: 5,
     description:
       "Experimenta una fusión de culturas y lenguajes en Coffee Portuguese and English. Este evento presencial te invita a sumergirte en el fascinante mundo de dos idiomas: portugués e inglés, mientras disfrutas de una deliciosa taza de café. Mejora tus habilidades lingüísticas, haz nuevos amigos internacionales y disfruta de conversaciones estimulantes en un ambiente acogedor. Únete a nuestra comunidad lingüística en Medellín y expande tus horizontes culturales mientras disfrutas de una experiencia enriquecedora. ¡Nos vemos allí!",
     date: "30-04-2024 21:00 GMT-5",
@@ -56,6 +60,7 @@ export const eventsData = [
     creatorUserId: 5,
     img: "/images/event5img.jpg",
     title: "5° Caminata Solidaria 'Siente la Vibra'",
+    category: 1,
     description:
       "Únete a la comunidad en la 5° edición de la Caminata Solidaria 'Siente la Vibra'. Es un evento que va más allá del ejercicio físico: es una oportunidad para unirnos en solidaridad y apoyo a aquellos que más lo necesitan. Camina con nosotros por las calles de Caracas mientras disfrutas de la energía contagiosa y el compañerismo de los participantes. Con cada paso que das, estarás contribuyendo a causas benéficas locales y ayudando a mejorar la vida de quienes nos rodean. Únete a nosotros y siente la vibra de la solidaridad en esta jornada única y significativa.",
     date: "05-05-2024 08:30 GMT-4",
@@ -68,6 +73,7 @@ export const eventsData = [
     id: 6,
     creatorUserId: 1,
     title: "Introducción a Dashboards con Shiny",
+    category: 3,
     description:
       "¡Aprende a crear visualizaciones interactivas impresionantes con Shiny! En este evento en línea, te introduciremos al emocionante mundo de los dashboards y te mostraremos cómo puedes utilizar Shiny, una herramienta poderosa en R, para crear aplicaciones web interactivas y dinámicas. Desde la importación y manipulación de datos hasta la creación de gráficos y tablas interactivas, este taller te proporcionará los conocimientos y habilidades necesarios para empezar a construir tus propios dashboards personalizados. Únete a nosotros y descubre cómo Shiny puede llevar tus visualizaciones de datos al siguiente nivel.",
     img: "/images/event6img.webp",
