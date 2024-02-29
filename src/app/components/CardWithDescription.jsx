faca45f444e33a0db7854595b506f2c663a1b4b4
Edgardosilva <edgardosilva.es@gmail.com>
2024-02-29 17:54:17 -0300

    add responsive to searchPage Page

--- src/app/components/CardWithDescription.jsx
+++ src/app/components/CardWithDescription.jsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 const CardWithDescription = ({ img, title, date, type, city, description }) => {
   return (
-    <div className="border border-slate-300 m-3 flex h-[240px] w-[42rem] bg-white rounded-lg shadow overflow-hidden">
-      <div className="flex-shrink-0 h-full w-64 relative">
+    <div className="border border-slate-300 m-3 flex h-[500px] md:h-[240px] md:w-[42rem] bg-white rounded-lg shadow overflow-hidden">
+      <div className="flex-shrink-0 h-full md:w-64 relative">
         <Image
           src={img || "/imgCard-1.png"}
           alt="card image"
@@ -13,11 +13,20 @@ const CardWithDescription = ({ img, title, date, type, city, description }) => {
           className="rounded-l-lg"
         />
       </div>
-      <div className="p-5 flex flex-col justify-center">
+      <div className="p-5 flex flex-col md:justify-center justify-between ">
+        <div className="md:hidden flex-shrink-0 h-48 relative">
+          <Image
+            src={img || "/imgCard-1.png"}
+            alt="card image"
+            layout="fill" 
+            objectFit="cover"
+            className="rounded-l-lg"
+          />
+        </div>
         <h5 className="mb-2 text-xl font-bold tracking-tight ">
           {title || "Titulo del evento"}
         </h5>
-        <p className="leading-5 h-22 overflow-y-hidden">
+        <p className="leading-5 h-24 md:h-22 overflow-y-scroll">
         {description || "No hay descripción"}
         </p>
         <div className="flex flex-col gap-2">
