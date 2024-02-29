984c6b7095a24e7b7a718361c6068ebdb872d253
Edgardosilva <edgardosilva.es@gmail.com>
2024-02-29 18:38:25 -0300

    Refactor EventFormMap component to use responsive styling

--- src/app/components/EventFormMap.jsx
+++ src/app/components/EventFormMap.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 
 const EventFormMap = ({ address }) => {
   return (
-    <article className=" h-[350px] rounded-lg p-3 mt-2">
+    <article className="h-[350px] rounded-lg md:p-3 md:mt-2 w-full relative">
       <iframe
-        width="500"
-        height="350"
+        width="100%"
+        height="100%"
         loading="lazy"
         allowFullScreen
         referrerPolicy="no-referrer-when-downgrade"
         src={`https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=${address}`}
-        className='rounded-lg'
+        className="rounded-lg absolute top-0 left-0 w-full h-full"
       ></iframe>
     </article>
   );
